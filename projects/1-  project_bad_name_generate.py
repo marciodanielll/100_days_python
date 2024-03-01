dd94b92b49294be5135edda828c054eba9660e32
M.D <marcio.daniel@msn.com>
2024-03-01 11:54:00 -0300

    feat: add project 1

---
+++ projects/1-  project_bad_name_generate.py
@@ -0,0 +1,14 @@
+#1. Create a greeting for your program.
+print('Hello my friends...')
+
+#2. Ask the user for the city that they grew up in.
+city_born = input('What city did you grow up in?\n')
+
+#3. Ask the user for the name of a pet.
+pet_name =  input('What is the name of your pet?\n')
+
+#4. Combine the name of their city and pet and show them their band name.
+combine_names =  city_born + ' ' +  pet_name
+
+#5. Make sure the input cursor shows on a new line:
+print(f'The new name is: {combine_names}')
